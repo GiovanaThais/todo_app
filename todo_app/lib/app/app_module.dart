b7cc92fd7f3867efcbdb9d5ad089fc2db266117a
GiovanaThais <giovanatgc12@gmail.com>
2023-12-14 20:36:21 -0300

    fix binds on app module

--- todo_app/lib/app/app_module.dart
+++ todo_app/lib/app/app_module.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:todo_app/app/core/repositories/task_repository.dart';
 import 'package:todo_app/app/core/services/local_storage/hive_local_storage_service.dart';
+import 'package:todo_app/app/core/services/local_storage/i_local_storage_service.dart';
 import 'package:todo_app/app/core/services/overlay/asuka_overlay_service.dart';
 import 'package:todo_app/app/core/services/overlay/i_overlay_service.dart';
 import 'package:todo_app/app/modules/home/home_module.dart';
@@ -9,11 +10,11 @@ import 'package:todo_app/app/modules/value_notifier/value_notifier_module.dart';
 class AppModule extends Module {
   @override
   void binds(Injector i) {
-    i.addLazySingleton<IOverlayService>((i) => AsukaOverLayService(i()),
+    i.addLazySingleton<IOverlayService>(() => AsukaOverLayService(i()),
         config: BindConfig(onDispose: (value) => value.dispose()));
 
-    i.addLazySingleton((i) => TaskRepository(i()));
-    i.addSingleton((i) => HiveLocalStorageService());
+    i.addLazySingleton(() => TaskRepository(i()));
+    i.addSingleton<ILocalStorageService>(() => HiveLocalStorageService());
   }
 
   @override
